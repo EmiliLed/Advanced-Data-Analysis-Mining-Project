7d4fe4cf74516c36b5d0766dadc834f35f3bc4c4
EmiliLed <emilie.ledoussal@lut.fi>
2025-10-10 14:13:18 +0300

    Update clean pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -7,7 +7,10 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from datetime import datetime
 now = datetime.now()
-import PLS
+import PLS_clean as PLS
+
+
+# --- Data Preparation ---
 
 def get_lagged_data(df_original_norm,lagging_rule): # lag data according to rules
     df_lagged = pd.DataFrame(index=df_original_norm.index)
@@ -26,6 +29,7 @@ def load_data_blocks_and_split(rule_apply=True,lag_rule=[2, 2, 2, 2, 2, 2, 2, 2,
     df2 = pd.read_csv("df_block210_09_11_55_21.csv")
     df3 = pd.read_csv("df_block310_09_11_55_21.csv")
     df4 = pd.read_csv("df_block410_09_11_55_21.csv")
+
     if rule_apply:
         df1_lagged=get_lagged_data(df1, lag_rule)
         df2_lagged = get_lagged_data(df2, lag_rule)
@@ -39,20 +43,18 @@ def load_data_blocks_and_split(rule_apply=True,lag_rule=[2, 2, 2, 2, 2, 2, 2, 2,
 
     return df1_lagged, df2_lagged, df3_lagged, df4_lagged
 
+
 def testing_timeseries_(x,model,x_columns,number_lags=9,name_y="% Silica Concentrate_mean_lag",simple=True):
     y_pred_array = []
     y_pred_to_x_array = []
     if not simple:
         for index, x_values in enumerate(x):
-            print(index)
-            print(y_pred_to_x_array)
             for i in range(len(y_pred_to_x_array)):
                 x_values[x_columns.get_loc(name_y + str(i+1))] = y_pred_to_x_array[i]
             y_pred = model.predict(x_values.reshape(1, -1)).item()
             y_pred_array.append(y_pred)
             y_pred_to_x_array = [y_pred] + y_pred_to_x_array
             while len(y_pred_to_x_array) > number_lags:
-                print("loop")
                 y_pred_to_x_array = y_pred_to_x_array[:number_lags]
 
         return y_pred_array
@@ -62,10 +64,9 @@ def testing_timeseries_(x,model,x_columns,number_lags=9,name_y="% Silica Concent
             y_pred_array.append(y_pred)
         return y_pred_array
 
-def model_testing(X_train,X_test,y_train,y_test,max_components_Lv,model=None):
+
+def model_testing(X_train,X_test,y_train,y_test,max_components_Lv,simple=True,model=None):
     x_col = X_test.columns
-    print(X_train.shape)
-    print(y_train.shape)
     X_train_std, X_mean, X_std = PLS.standardize(X_train.values)
     Y_train_std, Y_mean, Y_std = PLS.standardize(np.asarray(y_train).reshape(-1, 1))
     X_train_std = (X_train.values - X_mean) / X_std
@@ -79,7 +80,7 @@ def model_testing(X_train,X_test,y_train,y_test,max_components_Lv,model=None):
     if model is not None:
         y_pred_array = testing_timeseries_(X_test_std, model, x_col, simple=True, number_lags=9)
     else:
-        y_pred_array = testing_timeseries_(X_test_std, pls, x_col, simple=False, number_lags=9)
+        y_pred_array = testing_timeseries_(X_test_std, pls, x_col, simple=simple, number_lags=9)
 
     plt.figure(figsize=(10, 6))
     plt.scatter(Y_test_std, y_pred_array, alpha=0.7)
@@ -89,6 +90,13 @@ def model_testing(X_train,X_test,y_train,y_test,max_components_Lv,model=None):
     plt.title('PLS Regression: Actual vs Predicted')
     plt.grid()
     plt.show()
+
+    #Model evaluation
+    mse = mean_squared_error(Y_test_std, y_pred_array)
+    r2 = r2_score(Y_test_std, y_pred_array)
+    print(f'Mean Squared Error: {mse}')
+    print(f'R² Score: {r2}')
+    return pls,(X_mean,X_std),(Y_mean,Y_std)
 
 
 def execute_modelling():
@@ -101,10 +109,12 @@ def execute_modelling():
     df2_lagged_x, df2_lagged_y = df2_lagged.iloc[:, :-1], df2_lagged.iloc[:, -1]
     df3_lagged_x, df3_lagged_y = df3_lagged.iloc[:, :-1], df3_lagged.iloc[:, -1]
     df4_lagged_x, df4_lagged_y = df4_lagged.iloc[:, :-1], df4_lagged.iloc[:, -1]
+
     results = []
-    varaince_explined_x=[]
-    varaince_explined_y=[]
+    variance_explained_x=[]
+    variance_explained_y=[]
     test_size = 20
+
     for max_components_Lv in range(1,min(sum(lag_rule),max_components_Lv_)):
 
         X_train_1=pd.concat([df2_lagged_x, df3_lagged_x], ignore_index=True)
@@ -119,15 +129,18 @@ def execute_modelling():
         X_var = np.var(pls_2.x_scores_, axis=0)
         X_total_var = np.var(X_train_2, axis=0).sum()
         pctvar_X = X_var / X_total_var * 100
-        varaince_explined_x.append([max_components_Lv,pctvar_X])
+        variance_explained_x.append([max_components_Lv,pctvar_X])
+
         Y_var = np.var(pls_2.y_scores_, axis=0)
         Y_total_var = np.var(Y_train_2, axis=0).sum()
         pctvar_Y = Y_var / Y_total_var * 100
-        varaince_explined_y.append([max_components_Lv,pctvar_Y])
+
+        variance_explained_y.append([max_components_Lv,pctvar_Y])
         q2_mean=np.mean(np.vstack((np.asarray(q2_array_1),np.asarray(q2_array_2)))[:,3].mean())
         r2_mean = np.mean(np.vstack((np.asarray(r2_array_1), np.asarray(r2_array_2)))[:, 3].mean())
         mse_mean = np.mean(np.vstack((np.asarray(mse_array_1), np.asarray(mse_array_2)))[:, 3].mean())
         results.append([max_components_Lv,q2_mean,r2_mean,mse_mean])
+        
         print(q2_array_1,q2_array_2)
         print(f"Q² mean{q2_mean}")
         print(f"r2 mean{r2_mean}")
@@ -146,7 +159,8 @@ def execute_modelling():
     plt.figure(figsize=(8, 5))
     plt.plot(n_components, q2_scores, marker='o', linestyle='-', color='blue',label="Q2")
     plt.plot(n_components, mse_scores, marker='o', linestyle='-', color='green',label="MSE")
-    #plt.plot(n_components, r2_scores, marker='o', linestyle='-', color='red',label="R2")
+    plt.plot(n_components, r2_scores, marker='o', linestyle='-', color='red',label="R2")
+
     """print(results)
     
     for i in results:
@@ -157,8 +171,6 @@ def execute_modelling():
     print(n_components, q2_scores)"""
 
 
-
-
     plt.title("Q² vs Number of Latent Variables (LVs)")
     plt.xlabel("Number of Latent Variables")
     plt.ylabel("Q² Score")
@@ -167,6 +179,7 @@ def execute_modelling():
     plt.legend()
     plt.tight_layout()
     plt.show()
+
 
 def execute_testing():
     max_components_Lv=6;
@@ -179,8 +192,9 @@ def execute_testing():
     Y_train = pd.concat([df2_lagged_y, df3_lagged_y], ignore_index=True)
     X_test_1 = pd.concat([df4_lagged_x, df1_lagged_x], ignore_index=True)
     Y_test_1 = pd.concat([df4_lagged_y, df1_lagged_y], ignore_index=True)
-    model_testing(X_train,df4_lagged_x,Y_train,df4_lagged_y,max_components_Lv)
-    model_testing(X_train, df1_lagged_x, Y_train, df1_lagged_y, max_components_Lv)
+    #model_testing(X_train,df4_lagged_x,Y_train,df4_lagged_y,max_components_Lv,simple=False)
+    #model_testing(X_train, df1_lagged_x, Y_train, df1_lagged_y, max_components_Lv, simple=False)
+    model_testing(X_train,X_test_1,Y_train,Y_test_1,max_components_Lv)
 
 if __name__=='__main__':
     #execute_modelling()
